646e10178129bb4ceb70005e14d8b8e4d2964373
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2024-01-20 22:15:48 +0700

    fix: remove token expired

--- src/utils/general/general.js
+++ src/utils/general/general.js
@@ -9,11 +9,20 @@ const notificationSuccess = (message) => {
 };
 
 const notificationError = (message) => {
-    notification.error({
-      message: 'Opps!',
-      description: message ?  message.message : 'Mohon periksa kembali jaringan anda. Atau menghubungi call center',
-      placement: 'topRight',
-    });
+  
+  if(message.response.data.responseCode === `401`) {
+    localStorage.clear()
+    notificationSuccess('Anda berhasil logout!')
+    window.location.href = '/sign-in'
+  } else {
+    Object.keys(message.response.data.errors).forEach(error => {
+      notification.error({
+       message: 'Opps!',
+       description: message ?  error + ' ' + message.response.data.errors[error] : 'Mohon periksa kembali jaringan anda. Atau menghubungi call center',
+       placement: 'topRight',
+     });
+   });
+  }
 };
 
 export { 
